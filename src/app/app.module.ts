88372ba9cb6441f2ecd09ede320b849b8d5d4809
onur-cam <onurcam94@gmail.com>
2021-04-18 19:14:51 -0500

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { MatInputModule } from "@angular/material/input";
+import { TestTableComponent } from './test-table/test-table.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MatInputModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, TestTableComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
